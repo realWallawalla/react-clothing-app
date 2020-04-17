faa71484dbb0ef8aff5d214badd1a717819b51d6
Timon Sarakinis <timon.sarakinis@gmail.com>
2020-04-17 17:02:52 +0200

    added some basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
-
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+// Exact means that home page will render only when path is exact match. / is base address, in dev mode it localhost.3000.
+// Switch will only match one url at the same time.
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
