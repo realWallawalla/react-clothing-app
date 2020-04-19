30c01b03edac4b101ce2174e4a260d4c57d1a785
Timon Sarakinis <timon.sarakinis@gmail.com>
2020-04-19 15:12:39 +0200

    added header component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shoppage.component';
+import Header from './components/header/header.component';
 
 // Exact means that home page will render only when path is exact match. / is base address, in dev mode it localhost.3000.
 // Switch will only match one url at the same time.
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
