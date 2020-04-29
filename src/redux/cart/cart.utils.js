d060a9a224f354feea13ef1c6c108843f5e11e91
Timon Sarakinis <timon.sarakinis@gmail.com>
2020-04-29 14:24:45 +0200

    finished checkout page with adding additem and remove item

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -11,3 +11,16 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }] //if cartItemToAdd is new, attach quantity 1 to it and return with rest of items.
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    return cartItems.map(cartItem =>
+        cartItem.id === cartItemToRemove.id ?
+            { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem
+    );
+};
