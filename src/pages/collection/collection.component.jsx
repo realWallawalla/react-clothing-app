47779d182985e5fc77ce13b7ce3b1ccae3ed39bf
timon sarakinis <timon.sarakinis@gmail.com>
2020-07-23 15:52:45 +0200

    renamed category to collection. added some shoppage routeing

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+import './collection.styles.scss';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+const CollectionPage = ({ match }) => (
+    <div className='collection-page'>
+        <h2>CollectionPage PAGE</h2>
+    </div>
+)
+
+export default CollectionPage;
