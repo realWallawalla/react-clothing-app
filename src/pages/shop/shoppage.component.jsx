6c190e441c150d26dd1732c63e003ecfaaa62733
timon sarakinis <timon.sarakinis@gmail.com>
2020-07-23 15:29:46 +0200

    moved shoppage to a overview component

--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import CollectionPreview from '../../components/preview-collection/collection-preview.component';
-import { selectCollections } from '../../redux/shop/shop.selectors';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-
-
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
 const ShopPage = ({ collections }) => (
     <div className='shop-page'>
-        {
-            collections.map(({ id, ...otherCollectionsProps }) => (
-                <CollectionPreview key={id} {...otherCollectionsProps} />
-            ))
-        }
+        <CollectionsOverview />
     </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-})
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
