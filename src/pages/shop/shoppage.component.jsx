eae9f63b759e6c857692b515fbcf3692185b6ac3
timon sarakinis <timon.sarakinis@gmail.com>
2020-09-06 14:06:32 +0200

    trying out promeise pattern

--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -20,13 +20,13 @@ class ShopPage extends React.Component {
 
     componentDidMount() {
         const {updateCollections} = this.props;
-        const collectionRef = firestore.collection('collections')
+        const collectionRef = firestore.collection('collections');
 
-        collectionRef.onSnapshot(async snapshot => {
+        collectionRef.get().then(snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
             this.setState({loading: false});
-        })
+        });
     }
     render() {
         const { match } = this.props;
